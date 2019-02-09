d7217b709a03a34f490b9f2c17d856d00c7242a2
dmoneyballer <drew.gilliesbc@gmail.com>
2019-02-09 16:35:34 -0700

    added char encoding and removed console.logs

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -5,7 +5,6 @@ $("#alert").click(() => {
 setTimeout(function() {
   $('#urlCopyButton').click(function search() {
     var searchWord = document.querySelector('#searchWord').value;
-    console.log(searchWord);
     var results = [];
     $.ajax({
               url:`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&maxlag=5&search=${searchWord}&callback=?`,
@@ -18,7 +17,6 @@ setTimeout(function() {
                 for (var i = 0; i < data[1].length; i++) {
                   $("#output").append(`<li><a target="_blank" href= "${data[3][i]  } ">${data[1][i]}</a> ${ data[2][i]}</li>`);
                 }
-                console.log(data);
               },
               error: (err) =>{
                 console.log(err.responseJSON);
@@ -30,5 +28,4 @@ setTimeout(function() {
         $("#urlCopyButton").click();
       }
     })
-
 }, 310)
